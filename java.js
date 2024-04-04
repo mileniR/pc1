5e5f7940c78a15605a64522c76ed72f2c56b49e8
unknown <milreyesca@uch.pe>
2024-04-03 21:59:46 -0500

    segundo commit

--- java.js
+++ java.js
@@ -1,29 +1,57 @@
 
 const personasOficios = [
-    { nombre: "Juan", oficio: "Carpintero" },
-    { nombre: "María", oficio: "Electricista" },
-    { nombre: "Pedro", oficio: "Fontanero" },
-    { nombre: "Ana", oficio: "Programadora" }
-];
-
-
-function mostrarPersonasOficios() {
-    const container = document.getElementById("container");
-    container.innerHTML = ""; 
-
-    personasOficios.forEach(persona => {
-        const personaElement = document.createElement("div");
-        personaElement.innerHTML = `<strong>Nombre:</strong> ${persona.nombre}, <strong>Oficio:</strong> ${persona.oficio}`;
-        container.appendChild(personaElement);
-    });
-}
-
-function contratar() {
-    const indiceAleatorio = Math.floor(Math.random() * personasOficios.length);
-    const personaContratada = personasOficios[indiceAleatorio].nombre;
-    console.log("¡Contratado:", personaContratada, "!");
-}
-
-mostrarPersonasOficios();
-actionsButtons();
-document.getElementById("contratarBtn").addEventListener("click", contratar);
+    {
+     nombre: "Mileni ",
+     oficio: "Ingeniera de Sistemas"
+    },
+    {
+     nombre: "Jose",
+     oficio: "Bombero"
+    },
+    {
+     nombre: "Ana",
+     oficio: "Psicologa"
+    },   {
+     nombre: "Luis",
+     oficio: "Contador"
+    },   {
+     nombre: "Maria ",
+     oficio: "Capacitadora"
+    },
+ ];
+ 
+ const personasElement = document.getElementById("listapersonas");
+ 
+ function writePersona() {
+     let contenido = "";
+     personasOficios.forEach(persona => {
+         const template = `
+             <div class="persona">
+                 <div class="data">
+                 <h4>${persona.nombre}</h4>
+                 <h4>${persona.oficio}</h4>
+                 </div>
+                 <button class="btn" onclick="contratar('${persona.nombre}')">Contratar</button>            
+                 </div>
+         `;
+         contenido = contenido + template;
+     });
+     personasElement.innerHTML = contenido;
+ 
+ }
+ 
+ function contratar(nombre) {
+     Swal.fire({
+         title: 'Contratar',
+         text: `¿Seguro de contratar a ${nombre}?`,
+         icon: 'question',
+         showCancelButton: true,
+         confirmButtonText: 'Sí',
+         cancelButtonText: 'Cancelar'
+     }).then((result) => {
+         if (result.isConfirmed) {
+             Swal.fire('¡Contratado!', `${nombre} ha sido contratado.`, 'success');
+         }
+     });
+ }
+ writePersona();
